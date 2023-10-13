d955b73e5c6597d3129b5e267bf40f3f8f328c73
JosiasRodrigues <120478571+JosiasRodrigues@users.noreply.github.com>
2023-10-13 17:22:37 -0300

    atualizanção de responsividade

--- js/app.js
+++ js/app.js
@@ -606,7 +606,7 @@ cardapio.metodos = {
 cardapio.templates = {
 
     item: `
-    <div class="col-3 mb-5">
+    <div class="col-12 col-lg-3 col-md-3 col-sm-6 mb-5 fadeInUp">
     <div class="card card-item" id="\${id}">
         <div class="img-produto">
             <img src="\${img}"/>
